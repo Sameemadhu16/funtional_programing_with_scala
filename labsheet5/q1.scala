fe56916a1e19c65a02e2ee642e749747e59c7565
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-07-15 22:56:36 +0530

    update

--- labsheet5/q1.scala
+++ labsheet5/q1.scala
@@ -1,39 +1,25 @@
-import scala.io.StdIn.readLine
+object Fibonacci {
+  // Recursive function to calculate the nth Fibonacci number
+  def fib(n: Int): Int = {
+    if (n <= 0) 0
+    else if (n == 1) 1
+    else fib(n - 1) + fib(n - 2)
+  }
 
-object InventoryManagementSystem {
-
-  // Function to get the list of product names
-  def getProductList(): List[String] = {
-    var productList: List[String] = List()
-    var input: String = ""
-    println("Enter product names (type 'done' when finished):")
-    while (input != "done") {
-      input = readLine()
-      if (input != "done") {
-        productList = productList :+ input
+  // Recursive function to print the first n Fibonacci numbers
+  def printFibonacci(n: Int): Unit = {
+    def printFibonacciHelper(current: Int): Unit = {
+      if (current < n) {
+        println(fib(current))
+        printFibonacciHelper(current + 1)
       }
     }
-    productList
+    printFibonacciHelper(0)
   }
 
-  // Function to print the product list with positions
-  def printProductList(productList: List[String]): Unit = {
-    productList.zipWithIndex.foreach { case (product, index) =>
-      println(s"${index + 1}. $product")
-    }
-  }
-
-  // Function to get the total number of products
-  def getTotalProducts(productList: List[String]): Int = {
-    productList.length
-  }
-
-  // Main method to run the program
   def main(args: Array[String]): Unit = {
-    val products = getProductList()
-    println("\nProduct List:")
-    printProductList(products)
-    val totalProducts = getTotalProducts(products)
-    println(s"\nTotal number of products: $totalProducts")
+    val n = 10 // Change this value to print more or fewer Fibonacci numbers
+    println(s"First $n Fibonacci numbers:")
+    printFibonacci(n)
   }
 }
